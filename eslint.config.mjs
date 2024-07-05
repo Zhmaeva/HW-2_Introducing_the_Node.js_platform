80aaabbf4ec3e43fb53eb6f175536fffb39a156b
Ekaterina Zhmaeva <ekaterina.eduardovn@gmail.com>
2024-07-05 21:01:00 +0300

    first commit

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+export default [
+  {
+    rules: {
+      semi: "error",
+      "no-dupe-else-if": "error",
+      "no-duplicate-case": "error",
+      "no-unused-vars": "error",
+      "prefer-const": "error",
+      "no-duplicate-imports": "error",
+      "no-unreachable": "error",
+      "no-unused-vars": "warn",
+    },
+  },
+];
